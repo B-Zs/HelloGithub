58279ce2f10caaead054798eb14090f0265024d7
B-Zs <130469428+B-Zs@users.noreply.github.com>
2023-05-14 11:26:10 +0200

    Job prop added

--- HelloGithub/Person.cs
+++ HelloGithub/Person.cs
@@ -10,5 +10,7 @@ namespace HelloGithub
     {
         public string NAme { get; set; }
         public int Age { get; set; }
+        
+        public string Job {get;set;}//weboldalrol ideirva
     }
 }
